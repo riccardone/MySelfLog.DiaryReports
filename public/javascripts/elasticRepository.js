ce119d090bd74706e1c0874739dac337bba48d46
riccardone <riccardo@dinuzzo.it>
2018-01-21 19:57:20 +0000

    started

---
+++ public/javascripts/elasticRepository.js
@@ -0,0 +1,66 @@
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
+
+function ElasticRepository() {
+
+}
+
+function ping() {
+  client.ping({
+    // ping usually has a 3000ms timeout
+    requestTimeout: 1000
+  }, function (error) {
+    if (error) {
+      console.trace('elasticsearch cluster is down!');
+    } else {
+      console.log('All is well');
+    }
+  });
+}
+
+function search(correlationId, logType) {
+  return client.search({
+    index: 'diary-logs',
+    type: 'diaryLog',
+    q: 'CorrelationId: "' + correlationId + '" AND LogType: "' + logType + '"'
+  });
+}
+
+function getData(diaryName, logType) {
+  return client.search({
+    index: 'diary-events',
+    type: 'diaryEvent',
+    q: 'DiaryName: "' + diaryName + '"'
+  }).then((diaryEventResponse) => {
+    // TODO check if is an error or if there are hits before do that
+    var correlationId = diaryEventResponse.hits.hits[0]._source.Id;
+    return search(correlationId, logType);
+  }).then((diaryLogResponse) => {
+    var results = [];
+    diaryLogResponse.hits.hits.forEach(element => {
+      results.push({
+        "Id": element._source.Id,
+        "Value": element._source.Value,
+        "Mmolvalue": element._source.Mmolvalue,
+        "SlowTerapy": element._source.SlowTerapy,
+        "FastTerapy": element._source.FastTerapy,
+        "Calories": element._source.Calories,
+        "Message": element._source.Message,
+        "LogType": element._source.LogType,
+        "LogDate": element._source.LogDate,
+        "Source": element._source.Source,
+        "CorrelationId": element._source.CorrelationId,
+        "message": element._source.message
+      });
+    });
+    return results;
+  });
+}
+
+//ElasticRepository.prototype.search = search;
+ElasticRepository.prototype.getData = getData;
+ElasticRepository.prototype.ping = ping;
+module.exports = ElasticRepository;
