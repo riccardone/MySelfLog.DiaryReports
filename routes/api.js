a3e5d9d76eabd95e9645fc79b3bdd10baa47a617
riccardone <riccardo@dinuzzo.it>
2018-01-28 18:41:06 +0000

    added a simple unit test

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:diaryname/values/:format', function(req, res, next) {
-  res.send('TODO respond with a resource');
+  return res.send('TODO respond with a resource');
 });
 
 module.exports = router;
