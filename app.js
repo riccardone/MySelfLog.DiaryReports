b273c9aa3a6a2aebe22cbefc2ff79ce328ec5509
riccardone <riccardo@dinuzzo.it>
2018-03-03 11:48:38 +0000

    ...

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
+module.exports = function (config) {
+  var diaryRepository = (config && config.diaryRepository) || elasticDiaryRepository();
+  var routes = require('./routes')(diaryRepository);
+
+  var express = require('express');
+  var path = require('path');
+  var favicon = require('serve-favicon');
+  var logger = require('morgan');
+  var cookieParser = require('cookie-parser');
+  var bodyParser = require('body-parser');
+
+  var app = express();
+
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug');
+  app.locals.pretty = true;
+
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  app.get('/health', function (req, res) {
+    res.send('ok');
+  });
+  app.get('/stock/:isbn', routes.getCount);
+  app.post('/stock', routes.stockUp);
+
+  return app;
+};
+
+function elasticDiaryRepository() {
+  // TODO get the configs from env variables
+  var elasticRepositoryModule = require('../public/javascripts/elasticRepository');
+  return new elasticRepositoryModule();
+}
+
 require('dotenv').config()
 var express = require('express');
 var path = require('path');
@@ -32,14 +73,14 @@ app.use('/', index);
 app.use('/diary', diary);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -50,8 +91,8 @@ app.use(function(err, req, res, next) {
 });
 
 var server = app.listen(process.env.App_Port, process.env.App_Host, function () {
-  var address = server.address().address + ":" + server.address().port;  
-  console.log("App started on on %s", address)  
+  var address = server.address().address + ":" + server.address().port;
+  console.log("App started on on %s", address)
 });
 
 module.exports = server;
--- test/reports.test.js
+++ test/reports.test.js
@@ -1,14 +1,11 @@
 let chai = require('chai');
 let should = chai.should();
-let server = require('../app');
+var sinon = require('sinon')
+
+const fakeLogger = sinon.stub()
 
 describe('It should work', () => {
-  it('it should GET all the values', (done) => {
-    chai.request(server)
-      .get('/')
-      .end((err, res) => {
-        res.should.have.status(200);
-        done();
-      });
+  it('it should do something', (done) => {
+    
   });
 });
